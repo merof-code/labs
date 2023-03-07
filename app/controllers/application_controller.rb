cbf074c064f575c795c4a0c65718a8f3621357be
merof <misha.rosh@gmail.com>
2023-03-07 18:45:28 +0100

    locale and biiks new page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   # before_action :authenticate_user!
+  
+  # TODO: uncomment before_action :authenticate_user! and remove this
   def current_user
     User.new
   end
